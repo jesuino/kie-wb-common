bfe0b35eaeba84131cb5930a63c41c9683faedae
William Siqueira <wsiqueir@redhat.com>
2019-11-04 15:41:56 -0300

    Integration with Kogito Tooling ResourceContent API

--- kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-kogito-runtime/src/main/java/org/kie/workbench/common/stunner/kogito/client/services/WorkItemDefinitionStandaloneClientService.java
+++ kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-kogito-runtime/src/main/java/org/kie/workbench/common/stunner/kogito/client/services/WorkItemDefinitionStandaloneClientService.java
@@ -17,13 +17,18 @@
 package org.kie.workbench.common.stunner.kogito.client.services;
 
 import java.util.Collection;
+import java.util.Collections;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Default;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 
+import elemental2.dom.DomGlobal;
 import elemental2.promise.Promise;
+
+import org.appformer.kogito.bridge.client.resource.ResourceContentService;
 import org.kie.workbench.common.stunner.bpmn.client.workitem.WorkItemDefinitionClientService;
 import org.kie.workbench.common.stunner.bpmn.workitem.WorkItemDefinition;
 import org.kie.workbench.common.stunner.bpmn.workitem.WorkItemDefinitionCacheRegistry;
@@ -36,19 +41,58 @@ public class WorkItemDefinitionStandaloneClientService implements WorkItemDefini
 
     private final Promises promises;
     private final WorkItemDefinitionCacheRegistry registry;
+    private final WorkItemDefinitionParser parser;
+    private Promise<Collection<WorkItemDefinition>> fetchWIDsPromise;
+    private ResourceContentService resourceContentService;
+    private boolean loadedWids;
 
     @Inject
     public WorkItemDefinitionStandaloneClientService(final Promises promises,
-                                                     final WorkItemDefinitionCacheRegistry registry) {
+                                                     final WorkItemDefinitionCacheRegistry registry,
+                                                     final WorkItemDefinitionParser parser,
+                                                     final ResourceContentService resourceContentService) {
         this.promises = promises;
         this.registry = registry;
+        this.parser = parser;
+        this.resourceContentService = resourceContentService;
+    }
+    
+    @PostConstruct
+    public void loadWorkItemsDefinitions() {
+        // for some reason the call method is called twice, hence I did this sort of cache mechanism
+        // if it is called only one time so you don't need this
+        loadedWids = false;
+        fetchWIDsPromise = promises.create((success, failure) -> {
+           if (loadedWids) {
+               success.onInvoke(registry.items());
+           } else {
+               resourceContentService.list("**/*.wid").then(paths -> {
+                   if (paths.length > 0) {
+                       String path = paths[0];
+                       DomGlobal.console.log("Loading WIDs from " + path);
+                       resourceContentService.get(path).then(value -> {
+                           Collection<WorkItemDefinition> wids = parser.parse(value);
+                           wids.forEach(registry::register);
+                           loadedWids = true;
+                           success.onInvoke(wids);
+                           return null;
+                       });
+                   } else {
+                       DomGlobal.console.log("No WIDs to load");
+                       loadedWids = true;
+                       success.onInvoke(Collections.emptyList());
+                   }
+                   return null;
+               });
+           }
+        });
     }
 
     @Override
     public Promise<Collection<WorkItemDefinition>> call(final Metadata input) {
-        return promises.resolve(registry.items());
+        return fetchWIDsPromise;
     }
-
+    
     @Produces
     @Default
     @Override
